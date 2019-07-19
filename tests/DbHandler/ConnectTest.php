547c7923db0276117a4a8b0173223eed9a46a18f
Jacco-V <jacco.vinke@netsilik.nl>
2019-07-19 22:41:18 +0200

    Add basic test placeholder

---
+++ tests/DbHandler/ConnectTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Tests\Controllers\Login\ShowController;
+
+/**
+ * @package       netsilik/db-handler
+ * @copyright (c) 2011-2016 Netsilik (http://netsilik.nl)
+ * @license       EUPL-1.1 (European Union Public Licence, v1.1)
+ */
+
+use Tests\BaseTestCase;
+
+class ConnectTest extends BaseTestCase
+{
+    public function test_whenFoo_thenBar()
+    {
+        $this->assertTrue(true);
+    }
+}

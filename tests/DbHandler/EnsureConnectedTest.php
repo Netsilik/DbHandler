7577cb3e99da28ce5077ffa3ab046c9163252b8b
Jacco-V <jacco.vinke@netsilik.nl>
2019-07-20 17:54:51 +0200

    Add DbHandler::_ensureConnected() tests

---
+++ tests/DbHandler/EnsureConnectedTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace Tests\DbHandler;
+
+/**
+ * @package       netsilik/db-handler
+ * @copyright (c) 2011-2016 Netsilik (http://netsilik.nl)
+ * @license       EUPL-1.1 (European Union Public Licence, v1.1)
+ */
+
+use mysqli;
+use mysqli_stmt;
+use mysqli_result;
+use Tests\BaseTestCase;
+use Netsilik\DbHandler\DbHandler;
+use Tests\Mocks\FunctionOverwrites;
+
+
+class EnsureConnectedTest extends BaseTestCase
+{
+    public function test_whenNotConnected_thenNoticeTriggered()
+    {
+		$mMysqli_result = self::createMock(mysqli_result::class);
+		
+		$mMysqli_stmt = self::createMock(mysqli_stmt::class);
+		$mMysqli_stmt->method('execute')->willReturn(true);
+		$mMysqli_stmt->method('result_metadata')->willReturn($mMysqli_result);
+		
+		$mMysqli = self::createMock(mysqli::class);
+		$mMysqli->method('real_connect')->willReturn(true);
+		$mMysqli->method('ping')->willReturn( true, false);
+		$mMysqli->method('prepare')->willReturn($mMysqli_stmt);
+		
+		FunctionOverwrites::setActive('mysqli_init', $mMysqli);
+		
+		$dbHandler = new DbHandler('localhost', 'root', 'secret');
+
+
+		self::callInaccessibleMethod($dbHandler, '_ensureConnected');
+
+		self::assertErrorTriggered(E_USER_NOTICE, 'Reconnecting to DB');
+	}
+}

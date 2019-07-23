d38a1a83d57e6856c6ee55e93b5a08d607c299e5
Jacco Vinke <jvinke@motto.nl>
2019-07-23 13:41:00 +0200

    Add tests for DbStatementResult

---
+++ tests/DbResult/DbStatementResult/GetFieldCountTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace Tests\DbResult\AbstractDbResult;
+
+/**
+ * @package       netsilik/db-handler
+ * @copyright (c) 2011-2016 Netsilik (http://netsilik.nl)
+ * @license       EUPL-1.1 (European Union Public Licence, v1.1)
+ */
+
+use stdClass;
+use mysqli_stmt;
+use Tests\BaseTestCase;
+use Netsilik\DbHandler\DbResult\DbStatementResult;
+
+
+class GetFieldCountTest extends BaseTestCase
+{
+    public function test_whenMethodCalled_thenFieldCountReturned()
+    {
+    	$mMysqliStmt = self::createMock(mysqli_stmt::class);
+    	$mMysqliStmt->method('result_metadata')->willReturn(false);
+
+		$dbResult = new DbStatementResult($mMysqliStmt, 0.0);
+
+		$mStatement = new stdClass();
+		$mStatement->field_count = 123;
+		self::setInaccessibleProperty($dbResult, '_statement', $mStatement); // make sure we have a value (because: https://bugs.php.net/bug.php?id=63591)
+
+		$result = $dbResult->getFieldCount();
+	
+		self::assertEquals(123, $result);
+		
+		self::setInaccessibleProperty($dbResult, '_statement', $mMysqliStmt); // restore valid Mock
+	}
+    
+}
